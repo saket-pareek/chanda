fd10a4aba242e4d6b14ccf43f727da5512a28542
Saket <saket.wdev@gmail.com>
2022-04-08 00:30:03 +0530

    chhh

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlwuNlm1h92IrW-pr1rIVbhn__wfOEqCg",
+  authDomain: "chanda-db487.firebaseapp.com",
+  projectId: "chanda-db487",
+  storageBucket: "chanda-db487.appspot.com",
+  messagingSenderId: "951484055395",
+  appId: "1:951484055395:web:f9d7a8d0cce168aa485d56",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
